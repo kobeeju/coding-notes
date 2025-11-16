da00a81ae874bfb20fff1411e5bc11dd0d24371e
kobeeju <kobeeju26@gmail.com>
2025-11-17 00:50:24 +0900

    check 함수 작성

--- Python/로또_시뮬레이션/main.py
+++ Python/로또_시뮬레이션/main.py
@@ -29,6 +29,31 @@ def count_matching_numbers(numbers, winning_numbers):
     return count
 
 
+# 당첨금 확인
+def check(numbers, winning_numbers):
+    normal_winning_numbers = winning_numbers[:6]
+    bonus_winning_numbers = winning_numbers[6]
+    prize = 0
+
+    count = 0
+    for number in numbers:
+        if number in normal_winning_numbers:
+            count += 1
+
+    if count == 6:
+        prize = 1000000000
+    elif count == 5 and bonus_winning_numbers in numbers:
+        prize = 50000000
+    elif count == 5:
+        prize = 1000000
+    elif count == 4:
+        prize = 50000
+    elif count == 3:
+        prize = 5000
+
+    return prize
+
+
 # 테스트 코드
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [14]))
+print(check([2, 4, 11, 14, 25, 40], [4, 12, 14, 28, 40, 41, 6]))
+print(check([2, 4, 11, 14, 25, 40], [2, 4, 10, 11, 14, 40, 25]))
