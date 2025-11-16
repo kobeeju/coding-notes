030c55fc1ad56aec0d9274720958c8cf3336fc4f
kobeeju <kobeeju26@gmail.com>
2025-11-17 00:30:13 +0900

    draw_winning_numbers 함수 작성

--- Python/로또_시뮬레이션/main.py
+++ Python/로또_시뮬레이션/main.py
@@ -13,5 +13,19 @@ def generate_numbers(n):
     return numbers
 
 
+# 일반 당첨 번호 6개와 보너스 번호 1개 리스트 리턴
+def draw_winning_numbers():
+    winning_numbers = sorted(generate_numbers(6))
+
+    bonus_number = 0
+    while True:
+        bonus_number = generate_numbers(1)
+        if bonus_number not in winning_numbers:
+            winning_numbers += bonus_number
+            break
+
+    return winning_numbers
+
+
 # 테스트 코드
-print(generate_numbers(6))
+print(draw_winning_numbers())
