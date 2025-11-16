9f1f5e88d2fcddbded6a91d5eaa1eb2584e9c394
kobeeju <kobeeju26@gmail.com>
2025-11-16 23:36:53 +0900

    점수 계산 함수 작성

--- Python/숫자_야구/main.py
+++ Python/숫자_야구/main.py
@@ -33,4 +33,32 @@ def take_guess():
     return guesses
 
 
-print(take_guess())
+# 스트라이크 수와 볼 수 계산하기
+def get_score(guesses, solution):
+    strike_count = 0
+    ball_count = 0
+
+    for guess in guesses:
+        if guess in solution:
+            if guesses.index(guess) == solution.index(guess):
+                strike_count += 1
+            else:
+                ball_count += 1
+        else:
+            continue
+
+    return strike_count, ball_count
+
+
+# 테스트 코드
+s_1, b_1 = get_score([2, 7, 4], [2, 4, 7])
+print(s_1, b_1)
+
+s_2, b_2 = get_score([7, 2, 4], [2, 4, 7])
+print(s_2, b_2)
+
+s_3, b_3 = get_score([0, 4, 7], [2, 4, 7])
+print(s_3, b_3)
+
+s_4, b_4 = get_score([2, 4, 7], [2, 4, 7])
+print(s_4, b_4)
