e18382e322353e0f5e936d07d6b80305ae7475bc
kobeeju <kobeeju26@gmail.com>
2025-11-16 23:16:06 +0900

    코드 개선

--- Python/숫자_야구/main.py
+++ Python/숫자_야구/main.py
@@ -1,17 +1,16 @@
 from random import randint
 
 
+# 서로 다른 3개의 숫자 뽑기
 def generate_numbers():
     numbers = []
 
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
 
     while len(numbers) < 3:
-        number = randint(0, 9)
-        if number in numbers:
-            continue
-        else:
-            numbers.append(number)
+        new_number = randint(0, 9)
+        if new_number not in numbers:
+            numbers.append(new_number)
 
     return numbers
 
