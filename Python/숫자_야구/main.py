f7fdcfea0fe97c9a9b3f7b7be7c33a76d528861f
kobeeju <kobeeju26@gmail.com>
2025-11-16 23:27:08 +0900

    예측 숫자 입력받기

--- Python/숫자_야구/main.py
+++ Python/숫자_야구/main.py
@@ -15,4 +15,22 @@ def generate_numbers():
     return numbers
 
 
-print(generate_numbers())
+# 사용자로부터 3개의 추측 숫자를 입력받음
+def take_guess():
+    guesses = []
+
+    i = 1
+    while i <= 3:
+        guess = int(input(f"{i}번째 숫자를 입력하세요: "))
+        if guess < 0 or guess > 9:
+            print("범위를 벗어나는 숫자입니다. 다시 입력하세요.")
+        elif guess in guesses:
+            print("중복되는 숫자입니다. 다시 입력하세요.")
+        else:
+            guesses.append(guess)
+            i += 1
+
+    return guesses
+
+
+print(take_guess())
